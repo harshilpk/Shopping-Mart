8146b80a72d7f293d235ce6707d5ee6892dd7fa4
Harshil <harshilpk@gmail.com>
2019-02-11 02:14:31 -0700

    Serving html pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const path = require("path");
+
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    console.log("In another middleware");
-    res.send("<h1>Hello from express</h1>");
-  });
+router.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../', "views", "shop.html"));
+});
 
-
-module.exports = router
+module.exports = router;
