14c686ea7e11cda273763d631439a790cbed6c21
Harshil <harshilpk@gmail.com>
2019-02-11 01:51:50 -0700

    Using the Route feature of express

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // const http = require("http");
 const express = require("express");
 // const routes = require("./routes");
-
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require("./routes/shop");
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -9,20 +10,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/add-product',(req, res, next) => {
-    console.log("In another middleware");
-    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
-  });
+app.use(adminRoutes);
 
-app.use('/product', (req,res, nect) => {
-    console.log(req.body)
-    res.redirect('/');
-})
+app.use(shopRoutes);
 
-app.use('/',(req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from express</h1>");
-});
 // RAW LOGIC
 // const server = http.createServer(app);
 
