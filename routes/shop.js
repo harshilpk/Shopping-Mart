1fd80172595bc361c139be1fd2c42c4ec9cf3e58
Harshil <harshilpk@gmail.com>
2019-02-11 23:42:25 -0700

    Dynamic content using pug

--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,13 @@ const express = require("express");
 
 const router = express.Router();
 
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', "views", "shop.html"));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(__dirname, '../', "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path:"/" });
 });
 
 module.exports = router;
