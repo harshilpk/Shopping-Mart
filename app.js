65243b4f4dd4a6c190206b77596bf8594caff66f
Harshil <harshilpk@gmail.com>
2019-02-12 00:51:06 -0700

    Dynamic output using EJS

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const express = require("express");
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "handlebars",
-  expressHbs({
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-    extname: "handlebars"
-  })
-);
+// app.engine(
+//   "handlebars",
+//   expressHbs({
+//     layoutsDir: "views/layouts",
+//     defaultLayout: "main-layout",
+//     extname: "handlebars"
+//   })
+// );
 // app.set("view engine", "pug");
 // app.set("views", "views");
-app.set("view engine", "handlebars");
+// app.set("view engine", "handlebars");
+// app.set("views", "views");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
