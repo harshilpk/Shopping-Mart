3c28705f6499df1d83ff5254a006dba603e2e703
Harshil <harshilpk@gmail.com>
2019-02-13 00:13:10 -0700

    Edit Product functionality added

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,12 +2,10 @@ const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
@@ -17,17 +15,55 @@ exports.postAddProduct = (req, res, nect) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product =  new Product(title, imageUrl, description, price)
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
 
-exports.getProducts = (req,res,next) => {
-    Product.fetchAll(products => {
-        res.render("admin/products", {
-          prods: products,
-          pageTitle: "Admin Products",
-          path: "/admin/products"
-        });
-      });
-}
+exports.getEditProduct = (req, res, next) => {
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.id;
+  Product.findById(prodId, product => {
+    if (!product) {
+      console.log("I'm here");
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const productId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  const updatedProduct = new Product(
+    productId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products"
+    });
+  });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,14 @@ const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-
-
 router.get("/add-product", adminController.getAddProduct);
 
-router.get('/products', adminController.getProducts);
+router.get("/products", adminController.getProducts);
 
 router.post("/add-product", adminController.postAddProduct);
 
-module.exports = router;
+router.get("/edit-product/:id", adminController.getEditProduct);
 
+router.post('/edit-product', adminController.postEditProduct);
+
+module.exports = router;
