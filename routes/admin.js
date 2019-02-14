2832867606a24303f3f0281e34f3db5355cd0fef
Harshil <harshilpk@gmail.com>
2019-02-13 23:24:22 -0700

    Connected to MySQL

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 
-const adminController = require("../controllers/admin");
+const adminController = require("../controllers/admin(WIthoutDB)");
 
 const router = express.Router();
 
--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const shopController = require("../controllers/shop");
+const shopController = require("../controllers/shop(WithoutDB)");
 
 router.get("/", shopController.getIndex);
 
